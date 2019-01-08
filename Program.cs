71808f1fef91a5f7e71acf1c8e033036c7062912
Pushkar Desai <pushkar.desai999@hotmail.com>
2019-01-09 00:36:49 +0530

    jsEXecutor

--- Program.cs
+++ Program.cs
@@ -7,12 +7,19 @@ namespace myApp
     class Program
     {
         private static IWebDriver driver = new ChromeDriver(@"D:\Work\Advanced Selenium Libs\Drivers");
-
+        private static IJavaScriptExecutor jsExecutor= (IJavaScriptExecutor)driver;
         static void Main(string[] args) 
         {
-            driver.Navigate().GoToUrl("https://www.google.co.in");
-            driver.FindElement(By.Name("q")).SendKeys("abc");
-            driver.FindElement(By.Name("btnK")).Click();
+            try{
+                driver.Navigate().GoToUrl("https://www.google.co.in");
+                driver.FindElement(By.Name("q")).SendKeys("abc");
+                driver.FindElement(By.Name("q")).SendKeys(Keys.Enter);
+                jsExecutor.ExecuteScript("console.log('This is from jsExcecutor')");
+            }catch(Exception e){
+                Console.WriteLine(e);
+            }finally{
+                //driver.Close();
+            }
         }
     }
 }
