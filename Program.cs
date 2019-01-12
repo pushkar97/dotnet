1f0f495eefd67c7cef4b159e4d330f572c97754b
Pushkar Desai <pushkar.desai999@hotmail.com>
2019-01-12 19:55:04 +0530

    abc

--- Program.cs
+++ Program.cs
@@ -6,20 +6,31 @@ namespace myApp
 {
     class Program
     {
-        private static IWebDriver driver = new ChromeDriver(@"D:\Work\Advanced Selenium Libs\Drivers");
-        private static IJavaScriptExecutor jsExecutor= (IJavaScriptExecutor)driver;
+        // private static IWebDriver driver = new ChromeDriver(@"D:\Work\Advanced Selenium Libs\Drivers");
+        // private static IJavaScriptExecutor jsExecutor= (IJavaScriptExecutor)driver;
         static void Main(string[] args) 
         {
-            try{
-                driver.Navigate().GoToUrl("https://www.google.co.in");
+            using(IWebDriver driver = new ChromeDriver(@"D:\Work\Advanced Selenium Libs\Drivers")){
+                //driver.Navigate().GoToUrl("https://www.google.co.in");
+                driver.Url = "https://www.google.co.in";
                 driver.FindElement(By.Name("q")).SendKeys("abc");
                 driver.FindElement(By.Name("q")).SendKeys(Keys.Enter);
+
+                IJavaScriptExecutor jsExecutor= (IJavaScriptExecutor)driver;
                 jsExecutor.ExecuteScript("console.log('This is from jsExcecutor')");
-            }catch(Exception e){
-                Console.WriteLine(e);
-            }finally{
-                //driver.Close();
             }
+
+            // try{
+            //     //driver.Navigate().GoToUrl("https://www.google.co.in");
+            //     driver.Url = "https://www.google.co.in";
+            //     driver.FindElement(By.Name("q")).SendKeys("abc");
+            //     driver.FindElement(By.Name("q")).SendKeys(Keys.Enter);
+            //     jsExecutor.ExecuteScript("console.log('This is from jsExcecutor')");
+            // }catch(Exception e){
+            //     Console.WriteLine(e);
+            // }finally{
+            //     driver.Close();
+            // }
         }
     }
 }
